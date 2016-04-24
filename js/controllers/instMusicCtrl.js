4c6b2ff85e10c6a61b8086a97e3b073585d90e34
Natan <natanseverolp@gmail.com>
2016-04-24 16:30:41 -0300

    Concluindo Editar e Excluir da Aplicação

--- js/controllers/instMusicCtrl.js
+++ js/controllers/instMusicCtrl.js
@@ -14,4 +14,43 @@ angular.module("instMusic").controller("instMusicCtrl", function($scope){
     $scope.cadastroForm.$setPristine();
   };
 
+  //Editar, index
+  $scope.editarInstrumento = function(instrumentos){
+    $scope.instEditar = instrumentos.filter(function(instrumento){
+      if(instrumento.selecionado){
+        return instrumento;
+      }
+    });
+  };
+
+  $scope.habBotaoEditar = function(instrumentos){
+    var cont = 0;
+    var testeDeHab = instrumentos.filter(function(instrumento){
+      if(instrumento.selecionado){
+        cont++;
+      }
+    });
+    if(cont === 1){
+      return true;
+    } else {
+      return false;
+    }
+    // delete $scope.i;
+  };
+
+  //Excluir
+  $scope.excluirInstrumento = function(instrumentos){
+    $scope.instrumentos = instrumentos.filter(function(instrumento){
+      if(!instrumento.selecionado){
+        return instrumento;
+      }
+    });
+  };
+
+  $scope.habBotaoExcluir = function(instrumentos){
+    return instrumentos.some(function(instrumento){
+      return instrumento.selecionado;
+    });
+  };
+
 }); //Fim controller
